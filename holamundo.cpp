f04a45c846e55ab08edad0111e885430502fdeb5
andrew053003 <ux24nt283@ux.edu.mx>
2025-02-23 00:13:56 -0600

    Add files via upload
    EJERCICIOS CLASE 1
    FECHA : 22/02/25

---
+++ holamundo.cpp
@@ -0,0 +1,7 @@
+#include <iostream>   
+
+int main(int argc, char **argv) {
+    std::cout << "Hola Mundo!";  
+    return 0;
+} 
+
---
+++ ingresarvalores.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+int main()
+{
+	std::string nombre;
+	int edad;
+
+	std::cin >> nombre;
+	std::cin >> edad;
+
+	std::cout << "El nombre es "<< nombre << " de edad " << edad;
+	return 0;
+
+}
